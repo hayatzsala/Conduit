c286a49368316805459d953fe79485dfe55b8e1e
noor848 <54281955+noor848@users.noreply.github.com>
2022-10-20 14:13:40 +0300

    this update should be for task 3

--- Conduit/Conduit/Program.cs
+++ Conduit/Conduit/Program.cs
@@ -1,13 +1,5 @@
 using Conduit.Db;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Conduit.Db.Repositry;
-using Conduit.Service;
-using Conduit.Service.passwordHasher;
-using System.Configuration;
-using Conduit.helper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,30 +13,6 @@ builder.Services.AddDbContext<ConduitContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     // options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
-builder.Services.AddScoped<IAuthService,AuthService>();
-
-builder.Services.AddScoped<IpasswordHasher,BycryptPasswordHasher>();
-
-builder.Services.AddTransient<IUserRepositry,UserRepositry>();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.Configure<Jwt>(builder.Configuration.GetSection("JWT"));
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuerSigningKey = true,
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -56,7 +24,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
