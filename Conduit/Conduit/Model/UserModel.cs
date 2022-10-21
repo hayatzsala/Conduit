49f1e956016c0b180d43195a3cb3107f13e1845a
noor848 <54281955+noor848@users.noreply.github.com>
2022-10-22 01:09:05 +0300

    Article crud operation

---
+++ Conduit/Conduit/Model/UserModel.cs
@@ -0,0 +1,10 @@
+﻿using Conduit.Db;
+
+namespace Conduit.Model
+{
+    public class UserModel
+    {
+        public static string Username { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
