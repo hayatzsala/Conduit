6fb2e4985e2966f1c4adacd3af680470d7fa32f7
noor848 <54281955+noor848@users.noreply.github.com>
2022-10-22 02:26:43 +0300

    Follow user files

---
+++ Conduit/Conduit.Db/Repositry/FollowRepositry.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conduit.Db.Repositry
+{
+    public class FollowRepositry:IFollowRepositry
+    {
+        private readonly ConduitContext _context;
+
+        public FollowRepositry(ConduitContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> followAfriend(Guid userId,Guid FriendId)
+        {
+            await _context.Followers.AddAsync(
+
+               new Follower
+               {
+                  UserId = userId,
+                  FollowerId= FriendId
+
+               });
+            return await Save();
+
+        }
+
+        public async Task<bool> Save()
+        {
+            var saved = await _context.SaveChangesAsync();
+            return saved > 0 ? true : false;
+        }
+
+    }
+}
---
+++ Conduit/Conduit/Controllers/FollowController.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Conduit.Db;
+using Conduit.Dto;
+using Conduit.Model;
+using Conduit.Service;
+using Conduit.Service.passwordHasher;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Conduit.Controllers
+{
+    [Route("Api/[Controller]")]
+    [ApiController]
+    public class FollowController: Controller
+    {
+        public IConfiguration _configuration;
+        public IUserRepositry _userRepositry;
+        public IFollowRepositry _followRepositry;
+
+        public FollowController(IConfiguration configuration, IMapper mapper, IUserRepositry userRepositry, IFollowRepositry followRepositry = null)
+        {
+            _configuration = configuration;
+            _userRepositry = userRepositry;
+            _followRepositry = followRepositry;
+        }
+
+        [HttpPost("Follow/",Name ="AddFavourite")]
+        [Authorize]
+        public async Task<IActionResult> addAfreind(Guid FriendId)
+
+        {
+            var data= getTokenInformation();
+            var userID = await _userRepositry.GetUserID(data.Email);
+            var Follow =await _followRepositry.followAfriend(userID, FriendId);
+
+            if (Follow)
+            {
+                return Ok("Followed !");
+            }           
+            return BadRequest();
+        }
+
+        private AuthModel getTokenInformation()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                var AuthModel = new AuthModel
+                {
+                    Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                   Username= userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value
+            };
+
+                return AuthModel;
+            }
+            return null;
+
+        }
+
+
+    }
+}
+
