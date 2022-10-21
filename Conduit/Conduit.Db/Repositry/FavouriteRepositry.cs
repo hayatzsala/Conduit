e4456accc3c8543ba3cb4d1551879c74232ad5d3
noor848 <54281955+noor848@users.noreply.github.com>
2022-10-22 02:16:54 +0300

    create Favourite Article

---
+++ Conduit/Conduit.Db/Repositry/FavouriteRepositry.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conduit.Db.Repositry
+{
+    public class FavouriteRepositry:IFavouriteRepositry
+    {
+        private readonly ConduitContext _context;
+
+        public FavouriteRepositry(ConduitContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddFavourite(Guid userId ,Guid articleId)
+        {
+             await _context.Favourites.AddAsync(
+
+               new Favourite
+               {
+                  ArticleId = articleId,
+                  UserId = userId,
+             
+               });
+            return await Save();
+
+        }
+
+        public async Task<bool> Save()
+        {
+            var saved = await _context.SaveChangesAsync();
+            return saved > 0 ? true : false;
+        }
+
+    }
+}
---
+++ Conduit/Conduit/Controllers/FavouriteController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Conduit.Db;
+using Conduit.Dto;
+using Conduit.Model;
+using Conduit.Service;
+using Conduit.Service.passwordHasher;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Conduit.Controllers
+{
+    [Route("Api/[Controller]")]
+    [ApiController]
+    public class FavouriteController: Controller
+    {
+        public readonly IArticlesRepositry _ArticlesRepositry;
+        public IConfiguration _configuration;
+        public IMapper _mapper;
+        public IUserRepositry _userRepositry;
+        public IFavouriteRepositry _FavouriteRepositry;
+
+        public FavouriteController(IArticlesRepositry articlesRepositry, IConfiguration configuration, IMapper mapper, IUserRepositry userRepositry, IFavouriteRepositry favouriteRepositry)
+        {
+            _ArticlesRepositry = articlesRepositry;
+            _configuration = configuration;
+            _mapper = mapper;
+            _userRepositry = userRepositry;
+            _FavouriteRepositry = favouriteRepositry;
+        }
+
+        [HttpPost("Favourite/",Name ="AddFavourite")]
+        [Authorize]
+        public async Task<IActionResult> AddFavourite(Guid aricleId)
+
+        {
+            var data= getTokenInformation();
+            var CommentData=_mapper.Map<Comment>(Comment);
+            var userID = await _userRepositry.GetUserID(data.Email);
+            var FavouriteCreate =await _FavouriteRepositry.AddFavourite(userID,aricleId);
+
+            if (FavouriteCreate)
+            {
+                return Ok("Added Successfully !");
+            }           
+            return BadRequest();
+        }
+
+        private AuthModel getTokenInformation()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                var AuthModel = new AuthModel
+                {
+                    Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                   Username= userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value
+            };
+
+                return AuthModel;
+            }
+            return null;
+
+        }
+
+
+    }
+}
+
