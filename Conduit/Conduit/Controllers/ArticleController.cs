88c694d720c8df43c378720cc4445b1061ceb1df
noor848 <54281955+noor848@users.noreply.github.com>
2022-10-27 01:23:35 +0300

    main file edition

--- Conduit/Conduit/Controllers/ArticleController.cs
+++ Conduit/Conduit/Controllers/ArticleController.cs
@@ -21,25 +21,27 @@ namespace Conduit.Controllers
         public IConfiguration _configuration;
         public IMapper _mapper;
         public IUserRepositry _userRepositry;
-  
+        public IUserService _iuserService;
+        public ConduitContext _context;
 
-        public ArticlesController(IArticlesRepositry articlesRepositry, IConfiguration configuration, IMapper mapper,IUserRepositry userRepositry )
+
+        public ArticlesController(IArticlesRepositry articlesRepositry, IConfiguration configuration, IMapper mapper, IUserRepositry userRepositry, ConduitContext context)
         {
             _ArticlesRepositry = articlesRepositry;
             _configuration = configuration;
             _mapper = mapper;
             _userRepositry = userRepositry;
-          
+            _context = context;
+
         }
 
-        [HttpPost("Article/",Name ="CreateArticel")]
+        [HttpPost("Articles/")]
         [Authorize]
         public async Task<IActionResult> CreateArticle(ArticleD article)
 
         {
-            var data= getTokenInformation();
             var  artcleData=_mapper.Map<Article>(article);
-            var userID = await _userRepositry.GetUserID(data.Email);
+            var userID = new Guid(AuthModel.UserId);
             var userCreate =await _ArticlesRepositry.CreateArticle(artcleData,userID);
 
             if (userCreate)
@@ -48,8 +50,6 @@ namespace Conduit.Controllers
             }           
             return BadRequest();
         }
-
-
 
         [HttpPut("Article/", Name = "UpdateArticle")]
         [Authorize]
@@ -67,21 +67,33 @@ namespace Conduit.Controllers
 
         }
 
-        [HttpGet("/AllArticles/",Name ="GetAllArticles")]
+        [HttpGet("/AllArticle/{PageNumber}")]
         [Authorize]
-        public async Task<IActionResult> GetAllArticle()
+        public async Task<ActionResult<List<Article>>> GetAllArticle(int PageNumber)
         {
             var articles = await _ArticlesRepositry.GetAllArticle();
 
-            if (articles!=null)
+            if (articles != null)
             {
-                return Ok(articles);
-            }
+                var ArticlesNyumberInPage = 3f;
+                var pageCount = Math.Ceiling(_context.Articles.Count() / ArticlesNyumberInPage);
 
-            return BadRequest();
+                var articles = await _ArticlesRepositry.GetAllArticlePaginated(PageNumber, ArticlesNyumberInPage);
+
+                return Ok(
+                    new ArticleResponse
+                    {
+                        ArticlesList = articles,
+                        CurrentPage = PageNumber,
+                        PagesCount = (int)pageCount
+                    }
+                    );
+            }
+            return NotFound();
         }
 
-        [HttpGet("Article/", Name = "GetArticle")]
+
+        [HttpGet("Articles/")]
         [Authorize]
         public async Task<IActionResult> ReadArticle(Guid ArticleID)
         {
@@ -96,7 +108,7 @@ namespace Conduit.Controllers
         }
 
 
-        [HttpDelete("/Article/", Name = "DeleteArticle")]
+        [HttpDelete("/Articles/")]
         [Authorize]
         public async Task<IActionResult> DeleteArticle(Guid ArticleID)
         {
@@ -108,26 +120,6 @@ namespace Conduit.Controllers
             }
 
             return BadRequest();
-        }
-
-
-        private AuthModel getTokenInformation()
-        {
-            var identity = HttpContext.User.Identity as ClaimsIdentity;
-
-            if (identity != null)
-            {
-                var userClaims = identity.Claims;
-                var AuthModel = new AuthModel
-                {
-                    Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
-                   Username= userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value
-            };
-
-                return AuthModel;
-            }
-            return null;
-
         }
 
 
