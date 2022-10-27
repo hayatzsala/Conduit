2eec65ee5d749a049555b7be63726b4217bb87fa
noor848 <54281955+noor848@users.noreply.github.com>
2022-10-27 16:43:19 +0300

    edit the file

---
+++ Conduit/Conduit.Db/Dto/UserD.cs
@@ -0,0 +1,11 @@
+﻿namespace Conduit.Dto
+{
+    public class UserD
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public int Age { get; set; }
+    }
+}
